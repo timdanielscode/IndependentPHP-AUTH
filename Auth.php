243377acc80ea32eee0c689f2edaea60d5dc773b
timdanielscode <timdanielscode@hotmail.com>
2022-08-04 21:06:14 +0200

    Made the authorization optional
    Signed-off-by: timdanielscode <timdanielscode@hotmail.com>

--- Auth.php
+++ Auth.php
@@ -3,7 +3,7 @@
  * Auth 
  * 
  * @author Tim Daniëls
- * @version 0.1.0
+ * @version 0.2.0
  */
 namespace extensions;
 
@@ -23,27 +23,36 @@ class Auth {
      * @example authenticate(array('role' => 'normal')) || authenticate(array('role' => 'admin'))
      * @return bool true|false
      */
-    public static function authenticate($userRole) {
+    public static function authenticate($userRole = null) {
 
         $user = new User();
-        $user_role = new UserRole();
-        $role = new Roles();
+
         $request = new Request();
-
-        $userRole = $userRole['role'];
-
         $username = $request->get()['username'];
         $password = $request->get()['password'];
 
-        $sql = DB::try()->select($user->t.'.'.$user->id, $user->t.'.'.$user->username, $user->t.'.'.$user->password, $role->t.'.'.$role->name)->from($user->t)->join($user_role->t)->on($user->t.'.'.$user->id, '=', $user_role->t.'.'.$user_role->user_id)->join($role->t)->on($user_role->t.'.'.$user_role->role_id, '=', $role->t.'.'.$role->id)->where($user->username, '=', $username)->and($role->name, '=', $userRole)->first();
-        
+        if($userRole !== null) {
+
+            $user_role = new UserRole();
+            $role = new Roles();
+
+            $userRole = $userRole['role'];
+            $sql = DB::try()->select($user->t.'.'.$user->id, $user->t.'.'.$user->username, $user->t.'.'.$user->password, $role->t.'.'.$role->name)->from($user->t)->join($user_role->t)->on($user->t.'.'.$user->id, '=', $user_role->t.'.'.$user_role->user_id)->join($role->t)->on($user_role->t.'.'.$user_role->role_id, '=', $role->t.'.'.$role->id)->where($user->username, '=', $username)->and($role->name, '=', $userRole)->first();
+        } else {
+
+            $sql = DB::try()->select($user->t.'.'.$user->username, $user->t.'.'.$user->password)->from($user->t)->where($user->username, '=', $username)->first();
+            $sql['name'] = 'test';
+        }
+
         if(!empty($sql) && $sql !== null) {
 
             $fetched_password = $sql['password'];
             
             if(!password_verify($password, $fetched_password)) {
+
                 return false;
             } else {
+
                 Session::set('logged_in', true);
                 Session::set('user_role', $sql['name']);
                 Session::set('username', $sql['username']);
